0a27e7dfd87210c22f45c17de57cf8d63fe9cf7b
Ertugrul Karademir <ekarademir@gmail.com>
2019-10-02 09:21:33 +0100

    Added the script

---
+++ src/lattice_boltzmann.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+LATTICE_SIZE = 50
+
+
+def main():
+    rho = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    u = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n0 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n1 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n2 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n3 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n4 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n5 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n6 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n7 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    n8 = np.ndarray((LATTICE_SIZE, LATTICE_SIZE))
+    
+
+
+if __name__ == '__main__':
+    main()
